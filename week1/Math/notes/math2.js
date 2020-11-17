2f94ec41e7c808f990dd01877a87c2d19135d7b2
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2020-11-16 19:08:41 -0600

    nodePattern

--- week1/Math/notes/math2.js
+++
@@ -1,9 +0,0 @@
-// const _ = require('lodash')
-
-// let factor = 2
-
-// const mult = num => _.multiply(num, factor)
-
-// const setFactor = (newFactor) => factor = newFactor
-
-// module.exports = {mult, setFactor}
