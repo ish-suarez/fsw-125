d19bde6695a09b8c61444b05ab4e3d76c48d452b
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2020-11-17 20:25:18 -0600

    nodeModule

--- week1/Math/app.js
+++ week1/Math/app.js
@@ -1,6 +1,6 @@
 const {sum, subtract, multiply, divide} = require('./math.js')
 
-console.log(sum(2, 2))
-console.log(subtract(12, 24))
-console.log(multiply(20, 5))
-console.log(divide(121, 11))
+console.log(`Calculating Addition: ${sum(2, 2)}`)
+console.log(`Calculating Subtraction: ${subtract(12, 24)}`)
+console.log(`Calculating Multiplication: ${multiply(20, 5)}`)
+console.log(`Calculating Division: ${divide(121, 11)}`)
