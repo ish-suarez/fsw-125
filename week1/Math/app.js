876edcd8196f9c9426c5600eaf3db4bbfec69ed7
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2020-11-20 13:07:02 -0600

    Updated

--- week1/Math/app.js
+++ week1/Math/app.js
@@ -1,6 +1,6 @@
-const {sum, subtract, multiply, divide} = require('./math.js')
+const {sum, subtract, multiply, divide} = require('./math.js') // Imports The Functions From './Math'
 
-console.log(`Calculating Addition: ${sum(2, 2)}`)
-console.log(`Calculating Subtraction: ${subtract(12, 24)}`)
-console.log(`Calculating Multiplication: ${multiply(20, 5)}`)
-console.log(`Calculating Division: ${divide(121, 11)}`)
+console.log(`Calculating Addition: ${sum(2, 2)}`) // Calculates 4
+console.log(`Calculating Subtraction: ${subtract(12, 24)}`) // Calculates -12
+console.log(`Calculating Multiplication: ${multiply(20, 5)}`) // Calculates 100
+console.log(`Calculating Division: ${divide(121, 11)}`) // Calculates 11
--- week1/Math/math.js
+++ week1/Math/math.js
@@ -1,6 +1,6 @@
-sum = (a, b) => (a + b)
-subtract = (a, b) => (a - b)
-multiply = (a, b) => (a * b)
-divide = (a, b) => (a / b)
+sum = (a, b) => (a + b) // Function for Addition
+subtract = (a, b) => (a - b) // Function for Subtraction
+multiply = (a, b) => (a * b) // Function for Multiplication
+divide = (a, b) => (a / b) // Function for Division
 
-module.exports = {sum, subtract, multiply, divide}
+module.exports = {sum, subtract, multiply, divide} // Exporting Functions
